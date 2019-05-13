e339afc68b673b48f02eac904e04c6a58aacf5a1
Avery Khoo <averykhoo@gmail.com>
2019-05-13 18:02:53 +0800

    actually estimate smoothing is good

--- rmq_controller.py
+++ rmq_controller.py
@@ -288,11 +288,11 @@ class RMQ:
                 if eta < 0:
                     warnings.warn(f'queue count for <{",".join(queue_names)}> diverging from {target_value}')
 
-                # else:
-                #     # average over the last ten total-time estimates
-                #     estimates.append(eta + time.time() - _time_start)
-                #     estimate = sum(estimates[-_num_avg:]) / len(estimates[-_num_avg:])
-                #     eta = estimate + _time_start - time.time()
+                else:
+                    # average over the last few total-time estimates (20 seems like a good amount for my data)
+                    estimates.append(eta + time.time() - _time_start)
+                    estimate = sum(estimates[-_num_avg * 2:]) / len(estimates[-_num_avg * 2:])
+                    eta = estimate + _time_start - time.time()
             else:
                 eta = _eta_max
             eta = min(eta, _eta_max)
