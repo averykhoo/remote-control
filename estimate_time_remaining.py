cf45577bc8ebf9bcca70dfe79f701226bbc7eef6
Avery Khoo <averykhoo@gmail.com>
2019-05-14 18:10:25 +0800

    change smoothing

--- estimate_time_remaining.py
+++ estimate_time_remaining.py
@@ -6,21 +6,41 @@ import math
 
 def mean(vec):
     tmp = [elem for elem in vec if elem is not None and not math.isnan(elem)]
-    return sum(tmp) / len(tmp)
+    if len(tmp):
+        return float(sum(tmp) / len(tmp))
+    else:
+        return float('nan')
 
 
 class CompletionTimeEstimator:
+    rate: float
+    estimate: float
 
     def __init__(self, num_remaining, timestamp):
-        self.sample_size = 20
+        """
+        :param num_remaining: number of items left to process
+        :type num_remaining: int
+        :param timestamp: unix/windows timestamp or time.time()
+        :type timestamp: [int, float]
+        """
+
+        self.sample_size = 6
         self.smoothing_factor = 0.5  # exponential moving average
 
         self.count_history = [(num_remaining, timestamp)]
         self.moving_count_history = [(num_remaining, timestamp)]
-        self.rates = []
+        self.rate_history = []
+
+        self.rate = float('nan')
         self.estimate = float('nan')
 
     def update(self, num_remaining, timestamp):
+        """
+        :param num_remaining: number of items left to process
+        :type num_remaining: int
+        :param timestamp: unix/windows timestamp or time.time()
+        :type timestamp: [int, float]
+        """
 
         # calculate delta from last update
         last_n, last_t = self.count_history[-1]
@@ -28,8 +48,9 @@ class CompletionTimeEstimator:
         delta_t = timestamp - last_t
         assert delta_t > 0
 
+        # item count should not increase
         if delta_n < 0:
-            warnings.warn('item count increased, should only decrease')
+            warnings.warn('item count increased, it should only decrease')
 
         # update history
         self.count_history.append((num_remaining, timestamp))
@@ -40,66 +61,89 @@ class CompletionTimeEstimator:
             if len(self.moving_count_history) > 1:
                 first_n, first_t = self.moving_count_history[-(self.sample_size + 1):][0]
                 last_n, last_t = self.moving_count_history[-1]
-                self.rates.append(((first_n - last_n) / (last_t - first_t), timestamp))
+                self.rate_history.append(((first_n - last_n) / (last_t - first_t), timestamp))
 
         # no rate of change logged, stop here
-        if not self.rates:
+        if not self.rate_history:
             self.estimate = float('nan')
             return self.estimate
 
         # just use mean rate if less than 5 sample rates
-        if len(self.rates) <= 5:
-            rate = mean([r for r, t in self.rates])
+        if len(self.rate_history) < 5:
+            new_rate = mean([r for r, t in self.rate_history])
 
         # use weighted average by duration
         else:
             # total duration
-            t0 = self.rates[-(self.sample_size + 1):][0][-1]
-            rate_window_len = self.rates[-1][-1] - t0
+            t0 = self.rate_history[-(self.sample_size + 1):][0][-1]
+            rate_window_len = self.rate_history[-1][-1] - t0
             assert rate_window_len > 0
 
             # weighted average
             weighted_rates = []
-            for rate, t in self.rates[-(self.sample_size + 1):][1:]:
+            for rate, t in self.rate_history[-(self.sample_size + 1):][1:]:
                 weighted_rates.append(rate * (t - t0) / rate_window_len)
                 t0 = t
-            rate = sum(weighted_rates)
+            new_rate = sum(weighted_rates)
+
+        # moving exponential average for rate to prevent jumps
+        if math.isnan(self.rate):
+            self.rate = new_rate
+        else:
+            self.rate = self.rate * self.smoothing_factor + new_rate * (1 - self.smoothing_factor)
 
         # given the rate, what are the expected end times for past historical counts
         estimates = []
         for c, t in self.count_history[-self.sample_size:]:
-            estimates.append(t + c / rate)
-            # print(t, c, c/rate)
-        # print(estimates)
+            estimates.append(t + c / self.rate)
 
-        # moving exponential average for estimate to prevent jumps
-        if math.isnan(self.estimate):
-            self.estimate = mean(estimates)
-        else:
-            self.estimate = self.estimate * self.smoothing_factor + mean(estimates) * (1 - self.smoothing_factor)
-
+        # update and return estimated completion time (as timestamp)
+        self.estimate = mean(estimates)
         return self.estimate
 
 
 class RemainingTimeEstimator:
-    def __init__(self, num_remaining: [int, None] = None, name: [str, None] = None):
+    eta: float
+    estimate: float
+
+    def __init__(self, num_remaining=None, name=None):
+        """
+        :type num_remaining: [int, None]
+        :type name: [str, None]
+        """
+
         self.CTE = None
-        self.estimate = None
+        self.eta = float('nan')
+        self.estimate = float('nan')
         self.name = name
+        self.smoothing_factor = 0.1
 
         if num_remaining:
             self.update(num_remaining)
 
     def update(self, num_remaining):
+        """
+        :type num_remaining: int
+        """
         timestamp = time.time()
 
+        # create new completion time estimator
         if self.CTE is None:
             self.CTE = CompletionTimeEstimator(num_remaining, timestamp)
             self.estimate = float('nan')
             return self.estimate
 
-        completion_time = self.CTE.update(num_remaining, timestamp)
-        self.estimate = completion_time - timestamp
+        # get estimated time remaining
+        completion_estimate = self.CTE.update(num_remaining, timestamp)
+
+        # moving exponential average for estimate to prevent jumps
+        if math.isnan(self.eta):
+            self.eta = completion_estimate
+        else:
+            self.eta = self.eta * self.smoothing_factor + completion_estimate * (1 - self.smoothing_factor)
+
+        # actual remaining time
+        self.estimate = self.eta - timestamp
         return self.estimate
 
     def __str__(self):
