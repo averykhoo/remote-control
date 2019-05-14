58583203e0342fa1ce3da39c3ad331eff46e2ff4
Avery Khoo <averykhoo@gmail.com>
2019-05-14 16:00:03 +0800

    better format_seconds when less than 1 second

---
+++ estimate_time_remaining.py
@@ -0,0 +1,108 @@
+import time
+
+import math
+
+
+def mean(vec):
+    tmp = [elem for elem in vec if elem is not None and not math.isnan(elem)]
+    return sum(tmp) / len(tmp)
+
+
+class CompletionTimeEstimator:
+
+    def __init__(self, num_remaining, timestamp):
+        self.sample_size = 20
+        self.smoothing_factor = 0.5  # exponential moving average
+
+        self.count_history = [(num_remaining, timestamp)]
+        self.moving_count_history = [(num_remaining, timestamp)]
+        self.rates = []
+        self.estimate = float('nan')
+
+    def update(self, num_remaining, timestamp):
+
+        # calculate delta from last update
+        last_n, last_t = self.count_history[-1]
+        delta_n = last_n - num_remaining
+        delta_t = timestamp - last_t
+
+        # invariants
+        assert delta_n >= 0
+        assert delta_t > 0
+
+        # update history
+        self.count_history.append((num_remaining, timestamp))
+        if delta_n > 0:
+            self.moving_count_history.append((num_remaining, timestamp))
+
+            # recalculate rate
+            if len(self.moving_count_history) > 1:
+                first_n, first_t = self.moving_count_history[-(self.sample_size + 1):][0]
+                last_n, last_t = self.moving_count_history[-1]
+                self.rates.append(((first_n - last_n) / (last_t - first_t), timestamp))
+
+        # no rate of change logged, stop here
+        if not self.rates:
+            self.estimate = float('nan')
+            return self.estimate
+
+        # just use mean rate if less than 5 sample rates
+        if len(self.rates) <= 5:
+            rate = mean([r for r, t in self.rates])
+
+        # use weighted average by duration
+        else:
+            # total duration
+            t0 = self.rates[-(self.sample_size + 1):][0][-1]
+            rate_window_len = self.rates[-1][-1] - t0
+            assert rate_window_len > 0
+
+            # weighted average
+            weighted_rates = []
+            for rate, t in self.rates[-(self.sample_size + 1):][1:]:
+                weighted_rates.append(rate * (t - t0) / rate_window_len)
+                t0 = t
+            rate = sum(weighted_rates)
+
+        # given the rate, what are the expected end times for past historical counts
+        estimates = []
+        for c, t in self.count_history[-self.sample_size:]:
+            estimates.append(t + c / rate)
+            # print(t, c, c/rate)
+        # print(estimates)
+
+        # moving exponential average for estimate to prevent jumps
+        if math.isnan(self.estimate):
+            self.estimate = mean(estimates)
+        else:
+            self.estimate = self.estimate * self.smoothing_factor + mean(estimates) * (1 - self.smoothing_factor)
+
+        return self.estimate
+
+
+class RemainingTimeEstimator:
+    def __init__(self, num_remaining, name=None):
+        self.CTE = None
+        self.estimate = None
+        self.name = name
+
+        if num_remaining:
+            self.update(num_remaining)
+
+    def update(self, num_remaining):
+        timestamp = time.time()
+
+        if self.CTE is None:
+            self.CTE = CompletionTimeEstimator(num_remaining, timestamp)
+            self.estimate = float('nan')
+            return self.estimate
+
+        completion_time = self.CTE.update(num_remaining, timestamp)
+        self.estimate = completion_time - timestamp
+        return self.estimate
+
+    def __str__(self):
+        if self.name is None:
+            return f'RemainingTime<{self.estimate}>'
+        else:
+            return f'RemainingTime<[{self.name}]={self.estimate}>'
--- rmq_controller.py
+++ rmq_controller.py
@@ -7,29 +7,65 @@ import math
 import pika
 
 
-def format_seconds(num):
+def format_seconds(num_seconds):
     """
     string formatting
     note that the days in a month is kinda fuzzy
-    :type num: int | float
+    kind of takes leap years into account, but as a result the years are fuzzy
+    :type num_seconds: int | float
     """
-    num = abs(num)
-    if num == 0:
-        return u'0 seconds'
-    elif num == 1:
-        return u'1 second'
 
-    if num < 1:
-        # display 2 significant figures worth of decimals
-        return (u'%%0.%df seconds' % (1 - int(math.floor(math.log10(abs(num)))))) % num
+    # handle negatives
+    if num_seconds < 0:
+        minus = '-'
+    else:
+        minus = ''
+    num_seconds = abs(num_seconds)
 
-    unit = 0
-    denominators = [60.0, 60.0, 24.0, 7.0, 365.25 / 84.0, 12.0]
-    while unit < 6 and num > denominators[unit] * 0.9:
-        num /= denominators[unit]
-        unit += 1
-    unit = [u'seconds', u'minutes', u'hours', u'days', u'weeks', u'months', u'years'][unit]
-    return (u'%.2f %s' if num % 1 else u'%d %s') % (num, unit[:-1] if num == 1 else unit)
+    # zero (not compatible with decimals below)
+    if num_seconds == 0:
+        return '0 seconds'
+
+    # 1 or more seconds
+    if num_seconds >= 1:
+        unit = 0
+        denominators = [60.0, 60.0, 24.0, 7.0, 365.25 / 84.0, 12.0]
+        while unit < 6 and num_seconds > denominators[unit] * 0.9:
+            num_seconds /= denominators[unit]
+            unit += 1
+        unit = [u'seconds', u'minutes', u'hours', u'days', u'weeks', u'months', u'years'][unit]
+
+        # singular form
+        if num_seconds == 1:
+            unit = unit[:-1]
+
+        # exact or float
+        if num_seconds % 1:
+            return f'{minus}{num_seconds:,.2f} {unit}'
+        else:
+            return f'{minus}{num_seconds:,.0f} {unit}'
+
+    # fractions of a second (ms, μs, ns)
+    else:
+        unit = 0
+        while unit < 3 and num_seconds < 0.9:
+            num_seconds *= 1000
+            unit += 1
+        unit = [u'seconds', u'milliseconds', u'microseconds', u'nanoseconds'][unit]
+
+        # singular form
+        if num_seconds == 1:
+            unit = unit[:-1]
+
+        # exact or float
+        if num_seconds % 1 and num_seconds > 1:
+            return f'{minus}{num_seconds:,.2f} {unit}'
+        elif num_seconds % 1:
+            # noinspection PyStringFormat
+            num_seconds = f'{{N:,.{1 - int(math.floor(math.log10(abs(num_seconds))))}f}}'.format(N=num_seconds)
+            return f'{minus}{num_seconds} {unit}'
+        else:
+            return f'{minus}{num_seconds:,.0f} {unit}'
 
 
 class RChannel:
