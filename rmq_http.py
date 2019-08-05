9de1d8b1fead62a334701c78c60b3f43deff2ede
Avery Khoo <averykhoo@gmail.com>
2019-08-05 18:28:11 +0800

    update write_jsons

--- rmq_http.py
+++ rmq_http.py
@@ -1,4 +1,5 @@
 import json
+import warnings
 
 import requests
 from requests.auth import HTTPBasicAuth
@@ -33,10 +34,29 @@ class RMQ:
     def get_queue_info(self, queue_name):
         return self._api_get(f'/api/queues/{self.virtual_host}/{queue_name}')
 
-    def queue_insert(self, json_obj, queue_name):
+    def write_json(self, queue_name, json_obj):
         payload = {'properties':       {},
                    'routing_key':      queue_name,
                    'payload':          json.dumps(json_obj, ensure_ascii=False),
                    'payload_encoding': 'string',
                    }
         return self._api_post(f'/api/exchanges/{self.virtual_host}/{self.exchange}/publish', payload)
+
+    def write_jsons(self, queue_name, json_iterator):
+        for json_obj in json_iterator:
+            self.write_json(json_obj)
+
+    def is_alive(self):
+        result = self._api_get(f'/api/aliveness-test/{self.virtual_host}')
+        return result.get('status', None) == 'ok'
+
+    def health_check(self):
+        result = self._api_get(f'/api/healthchecks/node')
+        if result.get('status', None) == 'ok':
+            return True
+
+        warning_message = result.get('reason', None)
+        if warning_message is not None:
+            warnings.warn(warning_message)
+
+        return False
